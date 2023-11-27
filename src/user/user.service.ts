6b32ce31ec40356e972171c20a4e80cba392258f
mustafadurmaz <mdmz_001@hotmail.com>
2023-11-27 22:56:27 +0300

    create user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,18 +3,26 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository:Repository<User>) {}
-    activeUsers(){
-        return [{id:1,name: 'John'},{id:2,name: 'Mustafa'}]
-    }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  activeUsers() {
+    return [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Mustafa' },
+    ];
+  }
 
-    async create(CreateUserDto:CreateUserDto){
-        const result=await this.userRepository.create(CreateUserDto);
-        return {message:"Saved", CreateUserDto}
-    }
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.userRepository.create();
+    newUser.id = uuidv4();
+    newUser.email = createUserDto.email;
+    newUser.name = createUserDto.name;
+    return await this.userRepository.save(newUser);
+  }
 }
